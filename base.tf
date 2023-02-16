631bdb01d06135b661836a88f7d3d59eab4f5957
Pavel Pichrt <pav@pav.dev>
2023-02-16 10:55:11 +0000

    Fixed param name

--- base.tf
+++ base.tf
@@ -1,7 +1,7 @@
 module "base" {
   source = "pavelpichrt/codepipeline-trigger-base/aws"
 
-  type              = "ddb"
-  codepipeline_name = var.codepipeline_name
-  handler_zip_dir   = "${path.module}/handler"
+  type                       = "ddb"
+  codepipeline_name          = var.codepipeline_name
+  handler_implementation_dir = "${path.module}/handler"
 }
